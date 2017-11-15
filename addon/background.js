52165f6223ef8faa199c2e373374b74fb224fc06
Spawn38 <forum.spawn@gmail.com>
2017-11-15 22:41:47 +0100

    finish sending quality parameter

--- addon/background.js
+++ addon/background.js
@@ -1,65 +1,73 @@
-nativeName = "streamlinklauncher";
+nativeName = 'streamlinklauncher';
 
 function onCreated() {
   if (browser.runtime.lastError) {
-    console.log(`Error: ${browser.runtime.lastError}`);
+    console.log('Error: ${browser.runtime.lastError}');
   } else {
-    console.log("Item created successfully");
+    console.log('Item created successfully');
   }
 }
 
 function onRemoved() {
-  console.log("Item removed successfully");
+  console.log('Item removed successfully');
 }
 
 function onResponse(r) {
-  console.log("Response: " + r);
+  console.log('Response: ' + r);
 }
 
 function onError(error) {
-  console.log(`Error: ${error}`);
+  console.log('Error: ${error}');
 }
 
 function onDisconnect(p){
   p0 = p;
 }
 
-function onClickedAction(tab)  {
-  if(tab.url !== undefined && tab.url !== null){
-    console.log(tab.url);
-    s = browser.runtime.sendNativeMessage(nativeName, tab.url);
+function sendUrl(url) {
+  function sendMessage(result) {
+    var message = Object();
+    message.url = url;
+    message.quality = result.quality || 'best';
+    s = browser.runtime.sendNativeMessage(nativeName, message);
     s.then(onResponse, onError);
   }
+
+  browser.storage.local.get('quality').then(sendMessage, onError);
 }
 
 browser.menus.create({
-  id: "streamlinklauncher-link",
-  title: browser.i18n.getMessage("menuItemLinkStreamlink"),
-  contexts: ["link"],
+  id: 'streamlinklauncher-link',
+  title: browser.i18n.getMessage('menuItemLinkStreamlink'),
+  contexts: ['link'],
   targetUrlPatterns: ['*://go.twitch.tv/*','*://www.twitch.tv/*','*://www.dailymotion.com/video/*','*://livestream.com/accounts/*','*://www.youtube.com/watch?v=*']
 }, onCreated);
 
 browser.menus.create({
-  id: "streamlinklauncher-page",
-  title: browser.i18n.getMessage("menuItemPageStreamlink"),
-  contexts: ["page"],
+  id: 'streamlinklauncher-page',
+  title: browser.i18n.getMessage('menuItemPageStreamlink'),
+  contexts: ['page'],
   documentUrlPatterns: ['*://go.twitch.tv/*','*://www.twitch.tv/*','*://www.dailymotion.com/video/*','*://livestream.com/accounts/*','*://www.youtube.com/watch?v=*']
 }, onCreated);
 
 browser.menus.onClicked.addListener((info, tab) => {
   switch (info.menuItemId) {
-    case "streamlinklauncher-link":
+    case 'streamlinklauncher-link':
       url = info.linkUrl;
       break;
-    case "streamlinklauncher-page":
+    case 'streamlinklauncher-page':
       url = info.pageUrl;
       break;
   }
   if(url !== undefined && url !== null){
-    console.log(url);
-    s = browser.runtime.sendNativeMessage(nativeName, url);
-    s.then(onResponse, onError);
+    sendUrl(url);
   }
 });
+
+function onClickedAction(tab)  {
+  if(tab.url !== undefined && tab.url !== null){
+    sendUrl(tab.url);
+  }
+}
 
 browser.browserAction.onClicked.addListener(onClickedAction);
--- addon/options.js
+++ addon/options.js
@@ -1,27 +1,39 @@
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.local.set({
-    color: document.querySelector("#color").value
+    quality: document.getElementById('qualitySelect').value
   });
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    document.querySelector("#color").value = result.color || "blue";
+    document.getElementById('qualitySelect').value = result.quality || 'best';
   }
 
   function onError(error) {
-    console.log(`Error: ${error}`);
+    console.log('Error: ${error}');
   }
 
-  var getting = browser.storage.local.get("color");
+  var getting = browser.storage.local.get('quality');
   getting.then(setCurrentChoice, onError);
 }
 
-document.getElementById('submit').textContent=browser.i18n.getMessage("buttonSaveOption");
-document.getElementById('qualityLabel').textContent=browser.i18n.getMessage("labelQualityOption");
-document.getElementById('qualityDetail').textContent=browser.i18n.getMessage("detailQualityOption");
+document.getElementById('title').textContent=browser.i18n.getMessage('titleOption');
+document.getElementById('submit').textContent=browser.i18n.getMessage('buttonSaveOption');
+document.getElementById('qualityLabel').textContent=browser.i18n.getMessage('labelQualityOption');
+document.getElementById('qualityDetail').textContent=browser.i18n.getMessage('detailQualityOption');
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+var textQualityArray = browser.i18n.getMessage('listQualityOption').split(',');
+var valueQualityArray = browser.i18n.getMessage('valueQualityOption').split(',');
+var qualitySelect = document.getElementById('qualitySelect');
+
+textQualityArray.forEach(function(element, index) {
+  var option = document.createElement('option');
+  option.value = valueQualityArray[index];
+  option.text = element;
+  qualitySelect.add(option);
+});
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
